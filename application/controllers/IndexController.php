3c493a637f2eef7c37427ab27bd7f15b705f5733
rajshri1 <rajshri.deshpande@gmail.com>
2013-05-25 21:23:53 +0530

    Update IndexController.php

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -33,37 +33,47 @@ class IndexController extends Zend_Controller_Action
             
             if($param['twitterhandle'])
             {
-                if(($param['twitterhandle'] && $this->sessionValue['screen_name']) && ($param['twitterhandle'] != $this->sessionValue['screen_name']))
+                $followerArr = array();
+                if(!$this->sessionValue['screen_name'])
                 {
-                    Zend_Session::destroy();
+                    //Getting Entered User's Details
+                    $userDetailUrl = file_get_contents('https://api.twitter.com/1/users/show.json?screen_name='.$param['twitterhandle'].'&include_entities=true');
+                    $userDetails = json_decode($userDetailUrl);
+                    
+                    if($userDetails)
+                    {
+                        $this->view->twitterHandle = $userDetails->screen_name;
+                        $this->view->followerCount = $userDetails->followers_count;
+                    }
+                }
+                else
+                {
+                    $this->view->twitterHandle = $this->sessionValue['screen_name'];
+                    $this->view->followerCount = $this->sessionValue['followers_count'];
                 }
                 
                 if($this->sessionValue['followerArr'] > 0 && $param['cursor'] == '')
                 {
                     $followerArr = $this->sessionValue['followerArr'];
-                    $this->view->twitterHandle = $this->sessionValue['screen_name'];
-                    $this->view->followerCount = $this->sessionValue['followers_count'];
                     $this->view->nextCursor = $this->sessionValue['nextcursor'];
                 }
                 elseif(($this->sessionValue['followerArr'] > 0 && $param['cursor']) || !$this->sessionValue['followerArr'])
                 {
-                    //Getting Entered User's Details
-                    $userDetailUrl = file_get_contents('https://api.twitter.com/1/users/show.json?screen_name='.$param['twitterhandle'].'&include_entities=true');
-                    $userDetails = json_decode($userDetailUrl);
-                    if($userDetails)
+                    if(!$this->sessionValue['followerArr'])
                     {
-                        $this->view->twitterHandle = $followerSession['screen_name'] = $userDetails->screen_name;
-                        $this->view->followerCount = $followerSession['followers_count'] = $userDetails->followers_count;
-
-                        //Getting user's followers
-                        $jsonUrl = file_get_contents('http://api.twitter.com/1/statuses/followers/'.$followerSession['screen_name'].'.json?cursor='.$param['cursor']);
-                        $followerDetails = json_decode($jsonUrl);
-                        
-                        $this->view->nextCursor = $followerSession['nextcursor'] = $followerDetails->next_cursor_str;
-                        
-                        $followerSession['followerArr'] = $followerArr = (($this->sessionValue['followerArr'] > 0 )?array_merge($followerDetails->users,$this->sessionValue['followerArr']):$followerDetails->users);
-                        $this->_helper->Session()->session_val = $followerSession;
+                        unset($followerArr);
                     }
+                    //Getting user's followers
+                    $jsonUrl = file_get_contents('http://api.twitter.com/1/statuses/followers/'.$this->view->twitterHandle.'.json?cursor='.$param['cursor']);
+                    $followerDetails = json_decode($jsonUrl);
+                    
+                    $followerSession['screen_name'] = $this->view->twitterHandle;
+                    $followerSession['followers_count'] = $this->view->followerCount;
+                    $this->view->nextCursor = $followerSession['nextcursor'] = $followerDetails->next_cursor_str;
+                    
+                    $followerSession['followerArr'] = $followerArr = (($this->sessionValue['followerArr'] > 0 )?array_merge($followerDetails->users,$this->sessionValue['followerArr']):$followerDetails->users);
+                    $this->_helper->Session()->session_val = $followerSession;
+
                 }
                 
                 if(count($followerArr) > 0)
@@ -75,5 +85,15 @@ class IndexController extends Zend_Controller_Action
                 }
             }
         }
+    }
+    public function destroySessionAction()
+    {
+        $this->_helper->layout->disableLayout();
+        $this->_helper->viewRenderer->setNoRender(true);
+        
+        Zend_Session::destroy();
+        
+        $this->_redirect('/index');
+        exit;
     }
 }
